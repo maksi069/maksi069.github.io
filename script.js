437da2e3c02650a7bf6d344d3d0ac1f601a9ac5c
Maks <plminecraft069@gmail.com>
2018-10-29 22:35:28 +0100

    Now displaying fetched info! Minor style changes.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var x = document.getElementById("position");
+var x = document.getElementById("weatherInfo");
 
 	function getLocation() {
 
@@ -12,19 +12,20 @@ var x = document.getElementById("position");
 	function getWeather(position) {
 		var longitude = position.coords.longitude;
 		var latitude = position.coords.latitude;
-		//fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=4c03ce0b7b0c8b55220531dfed0f8c44").then(resp => resp.json())
-		//.then(resp => {
-	//			showWeather(resp);
-	//	})
-		var url = window.location.href;    
-		if (url.indexOf('?') > -1){
- 			url += '?lon=' + longitude + "&lat=" + latitude;
-		}else{
-			url += '?lon=' + longitude + "&lat=" + latitude; 
-		}
-		window.location.href = url;
+		fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=4c03ce0b7b0c8b55220531dfed0f8c44&units=metric").then(resp => resp.json())
+		.then(resp => {
+				showWeather(resp);
+		})
 	}
 
 	function showWeather(weather) {
 		console.log("test");
+		var content = document.getElementById("weatherInfo");
+		content.innerHTML = '<div id="weatherDiv"><p class="weatherHeader">' + weather.name + '</p>'
+		+ "<p>Current weather: " + weather.weather[0].main.toLowerCase() + "</p>"
+		+ "<p>Temperature: " + weather.main.temp + " &#176;C</p>"
+		+ "<p>Wind speed: " + weather.wind.speed + "</p>"
+		+ "<p>Pressure: " + weather.main.pressure + "</p>"
+		
+		+ '</div>';
 	}
